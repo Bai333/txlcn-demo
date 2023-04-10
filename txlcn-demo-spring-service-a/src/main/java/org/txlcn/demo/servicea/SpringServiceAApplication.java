d555d082cf20348bad4909ae26bbc845661e46c2
baiyc@kingser.com <baiyc@kingser.com>
2023-04-10 17:05:47 +0800

    优化：TX-LCN分布式管理框架顺利运行；

--- txlcn-demo-spring-service-a/src/main/java/org/txlcn/demo/servicea/SpringServiceAApplication.java
+++ txlcn-demo-spring-service-a/src/main/java/org/txlcn/demo/servicea/SpringServiceAApplication.java
@@ -14,7 +14,7 @@ import org.springframework.web.client.RestTemplate;
  * @author ujued
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient //代表这是服务注册中心的一个客户端，通用的服务注册注解 可以表示consul、zk、nacos 具体根据导入的依赖判断
 @EnableDistributedTransaction
 public class SpringServiceAApplication {
 
